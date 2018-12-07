a8ddf705d5689e145bcbb84f78552d742e7de84e
Vladimir Zhuravlev <izhur27@gmail.com>
2018-12-07 13:27:18 +0300

    Initial commit

---
+++ old/game-data.js
@@ -0,0 +1,26 @@
+var gameItems = [
+    {
+        "image": "item-1.png",
+        "name": "Apple iPhone 7 Plus 32GB",
+        "old-price": "54990 р",
+        "new-price": "46990 р",
+        "discount": "- 8000 &#8381;",
+        "border-color": "#009d9c"
+    },
+    {
+        "image": "item-2.png",
+        "name": "Телефон Samsung Galaxy J7",
+        "old-price": "17990 р",
+        "new-price": "14990 р",
+        "discount": "- 3000 &#8381;",
+        "border-color": "#ce8e38"
+    },
+    {
+        "image": "item-3.png",
+        "name": "Фитнес-браслет Xiaomi Mi Band 2",
+        "old-price": "2990 р",
+        "new-price": "1490 р",
+        "discount": "- 1500 &#8381;",
+        "border-color": "#cbd7d5"
+    }
+];
