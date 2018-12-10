24be5115cc6398594f069c460240c37054bb162b
Vladimir Zhuravlev <izhur27@gmail.com>
2018-12-10 13:31:06 +0300

    prices info & animation added

--- game-data.js
+++ game-data.js
@@ -2,24 +2,24 @@ var gameItems = [
     {
         "image": "item-1.png",
         "name": "Apple iPhone 7 Plus 32GB",
-        "old-price": "54990 р",
-        "new-price": "46990 р",
+        "old-price": "54990 &#8381;",
+        "new-price": "46 990 &#8381;",
         "discount": "- 8000 &#8381;",
         "border-color": "#009d9c"
     },
     {
         "image": "item-2.png",
         "name": "Телефон Samsung Galaxy J7",
-        "old-price": "17990 р",
-        "new-price": "14990 р",
+        "old-price": "17990 &#8381;",
+        "new-price": "14 990 &#8381;",
         "discount": "- 3000 &#8381;",
         "border-color": "#ce8e38"
     },
     {
         "image": "item-3.png",
         "name": "Фитнес-браслет Xiaomi Mi Band 2",
-        "old-price": "2990 р",
-        "new-price": "1490 р",
+        "old-price": "2990 &#8381;",
+        "new-price": "1 490 &#8381;",
         "discount": "- 1500 &#8381;",
         "border-color": "#cbd7d5"
     }
