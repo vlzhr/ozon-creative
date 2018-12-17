5aa1e52228f326f1573a8a4e19369e867ffbcf0b
Vladimir Zhuravlev <izhur27@gmail.com>
2018-12-17 23:28:56 +0100

    version without waves added

--- game-data.js
+++ game-data.js
@@ -2,24 +2,24 @@ var gameItems = [
     {
         "image": "item-1.png",
         "name": "Apple iPhone 7 Plus 32GB",
-        "old-price": "54990 &#8381;",
-        "new-price": "46 990 &#8381;",
+        "old-price": "54 990р",
+        "new-price": "46 990р",
         "discount": "- 8000 &#8381;",
         "border-color": "#009d9c"
     },
     {
         "image": "item-2.png",
         "name": "Телефон Samsung Galaxy J7",
-        "old-price": "17990 &#8381;",
-        "new-price": "14 990 &#8381;",
+        "old-price": "17 990р",
+        "new-price": "14 990р",
         "discount": "- 3000 &#8381;",
         "border-color": "#ce8e38"
     },
     {
         "image": "item-3.png",
         "name": "Фитнес-браслет Xiaomi Mi Band 2",
-        "old-price": "2990 &#8381;",
-        "new-price": "1 490 &#8381;",
+        "old-price": "2 990р",
+        "new-price": "1 490р",
         "discount": "- 1500 &#8381;",
         "border-color": "#cbd7d5"
     }
