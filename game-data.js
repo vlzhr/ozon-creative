e11b87bb2196561e4fec04d5728aba0c165d99ff
Vladimir Zhuravlev <izhur27@gmail.com>
2018-12-20 03:31:11 +0100

    new year theme added

--- game-data.js
+++ game-data.js
@@ -5,7 +5,8 @@ var gameItems = [
         "old-price": "54 990р",
         "new-price": "46 990р",
         "discount": "- 8000 &#8381;",
-        "border-color": "#009d9c"
+        "border-color": "#009d9c",
+        "present-image": "images/present-blue.png"
     },
     {
         "image": "item-2.png",
@@ -13,7 +14,8 @@ var gameItems = [
         "old-price": "17 990р",
         "new-price": "14 990р",
         "discount": "- 3000 &#8381;",
-        "border-color": "#ce8e38"
+        "border-color": "#ce8e38",
+        "present-image": "images/present-red.png"
     },
     {
         "image": "item-3.png",
@@ -21,6 +23,7 @@ var gameItems = [
         "old-price": "2 990р",
         "new-price": "1 490р",
         "discount": "- 1500 &#8381;",
-        "border-color": "#cbd7d5"
+        "border-color": "#cbd7d5",
+        "present-image": "images/present-green.png"
     }
 ];
