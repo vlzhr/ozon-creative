a02af566b1caf45bc6f73e94ec672f76479ab77d
Vladimir Zhuravlev <izhur27@gmail.com>
2018-12-21 14:19:42 +0100

    mobile bugs fixed; ny theme approved

--- game-data.js
+++ game-data.js
@@ -1,28 +1,28 @@
 var gameItems = [
     {
-        "image": "item-1.png",
+        "image": "items/7.png",
         "name": "Apple iPhone 7 Plus 32GB",
         "old-price": "54 990р",
-        "new-price": "46 990р",
-        "discount": "- 8000 &#8381;",
+        "new-price": "45 990р",
+        "discount": "-16%",
         "border-color": "#009d9c",
         "present-image": "images/present-blue.png"
     },
     {
-        "image": "item-2.png",
-        "name": "Телефон Samsung Galaxy J7",
-        "old-price": "17 990р",
-        "new-price": "14 990р",
-        "discount": "- 3000 &#8381;",
+        "image": "items/10.png",
+        "name": "Телефон Samsung Galaxy A8",
+        "old-price": "29 990Р",
+        "new-price": "22 990р",
+        "discount": "-23%",
         "border-color": "#ce8e38",
         "present-image": "images/present-red.png"
     },
     {
-        "image": "item-3.png",
+        "image": "items/8.png",
         "name": "Фитнес-браслет Xiaomi Mi Band 2",
-        "old-price": "2 990р",
+        "old-price": "1 990р",
         "new-price": "1 490р",
-        "discount": "- 1500 &#8381;",
+        "discount": "-25%",
         "border-color": "#cbd7d5",
         "present-image": "images/present-green.png"
     }
